e1d90335cccf67ebac29cddcdcd4300b7d2724a0
D-GU <dgodomg@gmail.com>
2022-09-10 15:32:44 +0300

    V 0.0

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,36 @@
-from numpy import load
+import torch
+import torchvision
+import numpy as np
+
+from itertools import chain
+from torch.utils.data import Dataset, DataLoader
 
 
 def get_data(_path: str):
-    return load(_path, allow_pickle=True)
+    return np.load(_path, allow_pickle=True)
 
+
+class ParametersDataset(Dataset):
+    def __init__(self, _path_x, _path_y):
+        _data_x = get_data(_path_x)
+        _data_y = get_data(_path_y)
+
+        _data_x = np.array(np.array([list(chain(*i)) for i in _data_x[::]]))
+
+        self.x = torch.from_numpy(_data_x)
+        self.y = torch.from_numpy(_data_y)
+        self.n_samples = self.x.shape[0]
+
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+
+    def __len__(self):
+        return self.n_samples
+
+
+if __name__ == "__main__":
+    dataset = ParametersDataset("train_ecg_parameters.npy", "train_y.npy")
+
+    first_data = dataset[1]
+    features, labels = dataset[0]
+    print(features)
