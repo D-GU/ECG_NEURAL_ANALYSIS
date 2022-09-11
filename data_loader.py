798a63c4c10fade9e25af06817c702250f9704f5
D-GU <dgodomg@gmail.com>
2022-09-11 17:40:17 +0300

    V 0.0

--- data_loader.py
+++ data_loader.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from itertools import chain
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 
 def get_data(_path: str):
@@ -15,14 +16,12 @@ class ParametersDataset(Dataset):
         _data_x = get_data(_path_x)
         _data_y = get_data(_path_y)
 
-        _data_x = np.array(np.array([list(chain(*i)) for i in _data_x[::]]))
-
-        self.x = torch.from_numpy(_data_x)
+        self.x = np.array([list(chain(*_data_x[i][::])) for i in range(17111)])
         self.y = torch.from_numpy(_data_y)
         self.n_samples = self.x.shape[0]
 
     def __getitem__(self, index):
-        return self.x[index], self.y[index]
+        return pad_sequence([torch.tensor(i) for i in self.x[index]], batch_first=True), self.y[index]
 
     def __len__(self):
         return self.n_samples
