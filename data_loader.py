32748b7f72c5f7655f3b433105c6c269e7d94ce5
D-GU <dgodomg@gmail.com>
2022-09-13 17:12:15 +0300

    V 0.0

--- data_loader.py
+++ data_loader.py
@@ -1,16 +1,28 @@
 import math
 
 import torch
-import torchvision
+import torch.nn as nn
 import numpy as np
+import torchvision
+import torch.nn.functional as F
 
 from math import ceil
 from itertools import chain
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
+from torchvision.transforms import ToTensor
 
-TRAIN_SIZE = 17111
+# Define hyper parameters
 VAL_SIZE = 2156
+TRAIN_SIZE = 17111
+INPUT_SIZE = 360
+HIDDEN_SIZE = 100
+NUM_CLASSES = 5
+NUM_EPOCHS = 2
+BATCH_SIZE = 4
+LEARNING_RATE = 0.01
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def get_data(_path: str):
@@ -23,26 +35,43 @@ class ParametersDataset(Dataset):
         _data_y = get_data(_path_y)
 
         self.x = np.array([list(chain(*_data_x[sample][::])) for sample in range(TRAIN_SIZE)])
-        self.y = torch.from_numpy(_data_y)
+        self.y = torch.tensor(_data_y)
         self.n_samples = _data_x.shape[0]
 
     def __getitem__(self, index):
-        return pad_sequence([torch.tensor(i) for i in self.x[index]], batch_first=True), self.y[index]
+        sample_x = np.array([sample for sample in self.x[index]])
+        tensor_sample_x = torch.tensor(sample_x)
+        return tensor_sample_x, self.y[index]
 
     def __len__(self):
         return self.n_samples
 
 
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(NeuralNet, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(hidden_size, 1)
+        self.softmax = nn.Softmax()
+
+    def forward(self, x):
+        out = self.linear1(x)
+        out = self.relu(out)
+        out = self.linear2(out)
+        out = self.softmax(out)
+
+        return out
+
+
 if __name__ == "__main__":
     dataset = ParametersDataset("train_ecg_parameters.npy", "train_y.npy")
-    dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True, num_workers=2)
+    dataloader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
 
-    # first_data = dataset
-    # features, labels = first_data[5]
-    # print(features)
+    n_iterations = ceil(TRAIN_SIZE / BATCH_SIZE)
 
-    num_epochs = 2
-    total_samples = len(dataset)
-    n_iterations = math.ceil(total_samples / 4)
-
-
+    # dummy loop
+    for epochs in range(NUM_EPOCHS):
+        for i, (inputs, labels) in enumerate(dataloader):
+            if (i + 1) % 5 == 0:
+                print(f"epoch {epochs + 1} / {NUM_EPOCHS}, step {i + 1}/ {n_iterations}, inputs {inputs.shape}")
