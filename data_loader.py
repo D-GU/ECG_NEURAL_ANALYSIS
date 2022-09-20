68331562e20fe408e31374d0e0ef674e78fb7cf4
D-GU <dgodomg@gmail.com>
2022-09-20 17:21:49 +0300

    Added up to six conv layers

--- data_loader.py
+++ data_loader.py
@@ -49,105 +49,95 @@ class ParametersDataset(Dataset):
         return self.n_samples
 
 
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.linear1 = nn.Linear(input_size, hidden_size)
+class Block(nn.Module):
+    def __init__(self, in_channel, identity_downsample=None, stride=1):
+        super(Block, self).__init__()
+        self.expansion = 4
+        self.conv1 = nn.LazyConv1d(in_channel, kernel_size=5, stride=1, padding=0)
+        self.bn1 = nn.LazyBatchNorm1d()
+        self.conv2 = nn.LazyConv1d(in_channel, kernel_size=5, stride=1, padding=0)
+        self.bn2 = nn.LazyBatchNorm1d()
+        self.conv3 = nn.LazyConv1d(in_channel, kernel_size=1, stride=1, padding=0)
+        self.bn3 = nn.LazyBatchNorm1d()
         self.relu = nn.ReLU()
-        self.linear2 = nn.Linear(hidden_size, num_classes)
+        self.identity_downsample = identity_downsample
 
     def forward(self, x):
-        out = self.linear1(x)
-        out = self.relu(out)
-        out = self.linear2(out)
+        identity = x
 
-        return out
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+
+        if self.identity_downsample:
+            identity = self.identity_downsample(identity)
+
+        x += identity
+
+        return self.relu(x)
 
 
-class ConvNeuralNet(nn.Module):
-    def __init__(self, in_channel, num_classes):
-        # It does something with the first layer on the first iteration
-        # Applied more convolution layers to check
-        super(ConvNeuralNet, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=in_channel, out_channels=180 - 5 + 1, kernel_size=21, stride=3)
-        self.pool = nn.MaxPool1d(2)
-        self.conv2 = nn.Conv1d(in_channels=180 - 5 + 1, out_channels=64, kernel_size=21, stride=3)
-        self.conv3 = nn.Conv1d(in_channels=64, out_channels=32, kernel_size=21, stride=3)
-        self.conv4 = nn.Conv1d(in_channels=32, out_channels=16, kernel_size=21, stride=3)
-        self.conv5 = nn.Conv1d(in_channels=16, out_channels=8, kernel_size=21, stride=3)
-        self.conv6 = nn.Conv1d(in_channels=8, out_channels=5, kernel_size=21, stride=3)
-        self.fc1 = nn.Linear(1, num_classes)
+class Resnet(nn.Module):  # [3, 4, 6, 3] - how many times to use blocks
+    def __init__(self, block, layers, channels, num_classes):
+        super(Resnet, self).__init__()
+
+        self.in_chanel = 2
+        self.conv1 = nn.LazyConv1d(channels, kernel_size=7, stride=1, padding=3)
+        self.bn1 = nn.LazyBatchNorm1d()
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool1d(1)
+
+        # ResNet layers
+        self.layer1 = self.make_layer(block, layers[0], stride=1)
+        self.layer2 = self.make_layer(block, layers[1], stride=2)
+        self.layer3 = self.make_layer(block, layers[2], stride=2)
+        self.layer4 = self.make_layer(block, layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.LazyLinear(num_classes)
 
     def forward(self, x):
-        # print(x)
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        # x = x.reshape(x.shape[0], -1)
-        x = self.fc1(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.reshape(x.shape[0], -1)
+
+        x = self.fc(x)
 
         return x
 
-    def backward(self, x):
-        pass
+    def make_layer(self, block, num_res_blocks, stride):
+        identity_downsample = None
+        layers = []
+
+        if stride != 1:
+            identity_downsample = nn.Sequential(
+                nn.LazyConv1d(self.in_chanel, kernel_size=1, stride=stride),
+                nn.LazyBatchNorm1d())
+
+        layers.append(block(self.in_chanel, identity_downsample, stride))
+
+        for i in range(num_res_blocks - 1):
+            layers.append(block(self.in_chanel))
+
+        return nn.Sequential(*layers)
 
 
-class Residual(nn.Module):  # @save
-    """The Residual block of ResNet."""
-
-    def __init__(self, num_channels, use_1x1conv=False, strides=1):
-        super().__init__()
-        self.conv1 = nn.LazyConv1d(num_channels, kernel_size=3, padding=1,
-                                   stride=strides)
-        self.conv2 = nn.LazyConv1d(num_channels, kernel_size=3, padding=1)
-        if use_1x1conv:
-            self.conv3 = nn.LazyConv1d(num_channels, kernel_size=1,
-                                       stride=strides)
-        else:
-            self.conv3 = None
-        self.bn1 = nn.LazyBatchNorm1d()
-        self.bn2 = nn.LazyBatchNorm1d()
-
-    def forward(self, X):
-        Y = F.relu(self.bn1(self.conv1(X)))
-        Y = self.bn2(self.conv2(Y))
-        if self.conv3:
-            X = self.conv3(X)
-        Y += X
-        return F.relu(Y)
-
-
-class ResNet(nn.Module):
-    def __init__(self, arch, lr=LEARNING_RATE, num_classes=5):
-        super(ResNet, self).__init__()
-        # self.save_hyperparameters()
-        self.net = nn.Sequential(self.b1())
-        for i, b in enumerate(arch):
-            self.net.add_module(f'b{i + 2}', self.block(*b, first_block=(i == 0)))
-        self.net.add_module('last', nn.Sequential(
-            nn.AdaptiveAvgPool1d(1), nn.Flatten(),
-            nn.LazyLinear(num_classes)))
-
-    def b1(self):
-        return nn.Sequential(
-            nn.LazyConv1d(2, kernel_size=7, stride=2, padding=3),
-            nn.LazyBatchNorm1d(), nn.ReLU(),
-            nn.MaxPool1d(kernel_size=3, stride=2, padding=1))
-
-    def block(self, num_residuals, num_channels, first_block=False):
-        blk = []
-        for i in range(num_residuals):
-            if i == 0 and not first_block:
-                blk.append(Residual(num_channels, use_1x1conv=True, strides=2))
-            else:
-                blk.append(Residual(num_channels))
-        return nn.Sequential(*blk)
-
-
-class ResNet18(ResNet):
-    def __init__(self, arch, lr=LEARNING_RATE, num_classes=5):
-        super().__init__(arch, lr, num_classes)
+def ResNet50(channels=2, num_classes=5):
+    return Resnet(Block, [3, 4, 6, 3], channels, num_classes)
 
 
 # Посчитать количество выходных слоев из одного слоя свертки (брать ширину свертки)
@@ -161,7 +151,7 @@ if __name__ == "__main__":
     n_iterations = ceil(TRAIN_SIZE / BATCH_SIZE)
 
     # model = NeuralNet(input_size=INPUT_SIZE, hidden_size=HIDDEN_SIZE, num_classes=NUM_CLASSES)
-    model = ResNet18(((2, 180), (2, 32)))
+    model = ResNet50()
 
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
@@ -172,7 +162,7 @@ if __name__ == "__main__":
 
             # Reshaping the input if needed
             # inputs = inputs.reshape(-1, 180 * 2).to(device)
-            # inputs = inputs.permute(0, 2, 1)
+            inputs = inputs.permute(0, 2, 1)
             # print("Input reshape - {}".format(inputs.shape))
 
             # fw
