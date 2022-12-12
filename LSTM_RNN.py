fe4ec4ba401b77b564055d89652e06946ce286c2
forest <dgodomg@gmail.com>
2022-12-12 12:22:16 +0300

    lstm nn

--- LSTM_RNN.py
+++ LSTM_RNN.py
@@ -31,6 +31,8 @@ class LSTM(pl.LightningModule):
         out = out[:, -1, :]
         out = self.fc(out)
 
+        return out
+
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
